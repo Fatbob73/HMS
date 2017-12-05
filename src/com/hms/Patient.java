8e12ad69a9bf575ab5c047c2fe5a7a433f29ede0
Fatbob73 <fatbob73@gmail.com>
2017-12-05 00:07:52 -0500

    Remove insured

--- src/com/hms/Patient.java
+++ src/com/hms/Patient.java
@@ -3,8 +3,6 @@ package com.hms;
 public class Patient extends User {
 
     private long patientId;
-    private boolean insured;
-    private HealthInsurancePlan insurancePlan;
 
     public long getPatientId() {
         return patientId;
@@ -12,22 +10,6 @@ public class Patient extends User {
 
     public void setPatientId(long patientId) {
         this.patientId = patientId;
-    }
-
-    public boolean isInsured() {
-        return insured;
-    }
-
-    public void setInsured(boolean insured) {
-        this.insured = insured;
-    }
-
-    public HealthInsurancePlan getInsurancePlan() {
-        return insurancePlan;
-    }
-
-    public void setInsurancePlan(HealthInsurancePlan insurancePlan) {
-        this.insurancePlan = insurancePlan;
     }
 
 }
